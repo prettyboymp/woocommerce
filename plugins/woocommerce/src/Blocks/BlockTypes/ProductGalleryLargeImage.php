fcdba9ee64ebd984654132250f0519670b550112
cpap <papazoglou.charalampos@gmail.com>
2025-03-20 14:06:24 +0200

    Fixes merge with trunk.

--- plugins/woocommerce/src/Blocks/BlockTypes/ProductGalleryLargeImage.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/ProductGalleryLargeImage.php
@@ -140,6 +140,7 @@ class ProductGalleryLargeImage extends AbstractBlock {
 							data-wp-on--touchmove="actions.onTouchMove"
 							data-wp-on--touchend="actions.onTouchEnd"
 							data-wp-watch="callbacks.toggleImageActiveClass"
+							loading="lazy"
 							alt=""
 						/>
 					</li>
