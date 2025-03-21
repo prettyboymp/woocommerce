a06f601d99eba72c0676e8ace922b33f81cbe589
Tom Cafferkey <tjcafferkey@gmail.com>
2025-03-21 15:26:24 +0000

    Create a recursive function to check product gallery inner blocks for required classnames

--- plugins/woocommerce/client/blocks/assets/js/blocks/product-gallery/save.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/product-gallery/save.tsx
@@ -2,11 +2,62 @@
  * External dependencies
  */
 import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
+import { rawHandler, serialize } from '@wordpress/blocks';
+
+const GROUP_BLOCK_NAMES_AND_CLASS_NAMES_MAP = {
+	'Gallery Area': 'wc-block-product-gallery__gallery-area',
+	'Large Image and Navigation': 'wc-block-product-gallery__large-image-and-navigation',
+};
+
+function addGalleryClassNames(blocks: any[]): any[] {
+    return blocks.map(block => {
+        const updatedBlock = { ...block };
+        
+        // Check if this block's name exists in our mapping
+        const blockName = block.attributes?.metadata?.name;
+        const className = GROUP_BLOCK_NAMES_AND_CLASS_NAMES_MAP[blockName as keyof typeof GROUP_BLOCK_NAMES_AND_CLASS_NAMES_MAP];
+        
+        if (className) {
+            // Add className if it doesn't exist
+            if (!block.attributes.className?.includes(className)) {
+                updatedBlock.attributes = {
+                    ...updatedBlock.attributes,
+                    className: block.attributes.className
+                        ? `${block.attributes.className} ${className}`
+                        : className
+                };
+            }
+        }
+
+        // Recursively process innerBlocks if they exist
+        if (block.innerBlocks && block.innerBlocks.length > 0) {
+            updatedBlock.innerBlocks = addGalleryClassNames(block.innerBlocks);
+        }
+
+        return updatedBlock;
+    });
+}
 
 export const Save = () => {
-	const blockProps = useBlockProps.save( {
+	const blockProps = useBlockProps.save({
 		className: 'wc-block-product-gallery',
-	} );
-	const innerBlocksProps = useInnerBlocksProps.save( blockProps );
-	return <div { ...innerBlocksProps } />;
+	});
+	const innerBlocksProps = useInnerBlocksProps.save(blockProps);
+	const parsedBlocks = rawHandler({ HTML: innerBlocksProps.children.props.children });
+	const updatedParsedBlocks = addGalleryClassNames(parsedBlocks);
+	const serializedUpdatedBlocks = serialize(updatedParsedBlocks);
+
+	// Create new props object with the updated children
+	const updatedInnerBlocksProps = {
+		...innerBlocksProps,
+		children: {
+			...innerBlocksProps.children,
+			props: {
+				...innerBlocksProps.children.props,
+				children: serializedUpdatedBlocks
+			}
+		}
+	};
+
+	return <div {...updatedInnerBlocksProps} />;
 };
