919a116870480a042d5608c14a978b883814ae39
Tom Cafferkey <tjcafferkey@gmail.com>
2025-03-21 16:13:13 +0000

    Fix formatting

--- plugins/woocommerce/client/blocks/assets/js/blocks/product-gallery/save.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/product-gallery/save.tsx
@@ -9,7 +9,8 @@ import { rawHandler, serialize } from '@wordpress/blocks';
  */
 const GROUP_BLOCK_NAMES_AND_CLASS_NAMES_MAP = {
 	'Gallery Area': 'wc-block-product-gallery__gallery-area',
-	'Large Image and Navigation': 'wc-block-product-gallery__large-image-and-navigation',
+	'Large Image and Navigation':
+		'wc-block-product-gallery__large-image-and-navigation',
 };
 
 /**
@@ -17,48 +18,55 @@ const GROUP_BLOCK_NAMES_AND_CLASS_NAMES_MAP = {
  * @param blocks - The blocks to add class names to
  * @returns The blocks with the added class names
  */
-function addGalleryClassNames(blocks: any[]): any[] {
-    return blocks.map(block => {
-        const updatedBlock = { ...block };
-        
-        // Check if this block's name exists in our mapping
-        const blockName = block.attributes?.metadata?.name;
+function addGalleryClassNames( blocks: any[] ): any[] {
+	return blocks.map( ( block ) => {
+		const updatedBlock = { ...block };
 
-        if ( !blockName ) {
-            return updatedBlock;
-        }
+		// Check if this block's name exists in our mapping
+		const blockName = block.attributes?.metadata?.name;
 
-        const className = GROUP_BLOCK_NAMES_AND_CLASS_NAMES_MAP[blockName as keyof typeof GROUP_BLOCK_NAMES_AND_CLASS_NAMES_MAP];
-        
-        if ( className ) {
-            // Add className if it doesn't exist
-            if (!block.attributes.className?.includes(className)) {
-                updatedBlock.attributes = {
-                    ...updatedBlock.attributes,
-                    className: block.attributes.className
-                        ? `${block.attributes.className} ${className}`
-                        : className
-                };
-            }
-        }
+		if ( ! blockName ) {
+			return updatedBlock;
+		}
 
-        // Recursively process innerBlocks if they exist
-        if (block.innerBlocks && block.innerBlocks.length > 0) {
-            updatedBlock.innerBlocks = addGalleryClassNames(block.innerBlocks);
-        }
+		const className =
+			GROUP_BLOCK_NAMES_AND_CLASS_NAMES_MAP[
+				blockName as keyof typeof GROUP_BLOCK_NAMES_AND_CLASS_NAMES_MAP
+			];
 
-        return updatedBlock;
-    });
+		if ( className ) {
+			// Add className if it doesn't exist
+			if ( ! block.attributes.className?.includes( className ) ) {
+				updatedBlock.attributes = {
+					...updatedBlock.attributes,
+					className: block.attributes.className
+						? `${ block.attributes.className } ${ className }`
+						: className,
+				};
+			}
+		}
+
+		// Recursively process innerBlocks if they exist
+		if ( block.innerBlocks && block.innerBlocks.length > 0 ) {
+			updatedBlock.innerBlocks = addGalleryClassNames(
+				block.innerBlocks
+			);
+		}
+
+		return updatedBlock;
+	} );
 }
 
 export const Save = () => {
-	const blockProps = useBlockProps.save({
+	const blockProps = useBlockProps.save( {
 		className: 'wc-block-product-gallery',
-	});
-	const innerBlocksProps = useInnerBlocksProps.save(blockProps);
-	const parsedBlocks = rawHandler({ HTML: innerBlocksProps.children.props.children });
-	const updatedParsedBlocks = addGalleryClassNames(parsedBlocks);
-	const serializedUpdatedBlocks = serialize(updatedParsedBlocks);
+	} );
+	const innerBlocksProps = useInnerBlocksProps.save( blockProps );
+	const parsedBlocks = rawHandler( {
+		HTML: innerBlocksProps.children.props.children,
+	} );
+	const updatedParsedBlocks = addGalleryClassNames( parsedBlocks );
+	const serializedUpdatedBlocks = serialize( updatedParsedBlocks );
 
 	// Create new props object with the updated children
 	const updatedInnerBlocksProps = {
@@ -67,10 +75,10 @@ export const Save = () => {
 			...innerBlocksProps.children,
 			props: {
 				...innerBlocksProps.children.props,
-				children: serializedUpdatedBlocks
-			}
-		}
+				children: serializedUpdatedBlocks,
+			},
+		},
 	};
 
-	return <div {...updatedInnerBlocksProps} />;
+	return <div { ...updatedInnerBlocksProps } />;
 };
