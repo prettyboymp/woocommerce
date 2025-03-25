2dcfa3bd59c5f403a2edc40dd5e81f1fe7e81209
Daniel Mallory <daniel.mallory@automattic.com>
2025-03-25 11:10:21 +0000

    Properly typing the Children ReactNode.|

--- plugins/woocommerce/client/admin/client/settings-payments/components/payment-settings-layout/payment-settings-layout.tsx
+++ plugins/woocommerce/client/admin/client/settings-payments/components/payment-settings-layout/payment-settings-layout.tsx
@@ -1,15 +1,14 @@
 /**
  * External dependencies
  */
-import React from 'react';
 
 /**
  * Internal dependencies
  */
 import './payment-settings-layout.scss';
 
-export const PaymentSettingsLayout = ( { children } ) => (
-	<div className="payment-settings-layout">{ children }</div>
-);
-
-export default PaymentSettingsLayout;
+export const PaymentSettingsLayout = ( {
+	children,
+}: {
+	children: React.ReactNode;
+} ) => <div className="payment-settings-layout">{ children }</div>;
