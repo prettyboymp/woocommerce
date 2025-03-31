ad0bd9c6df02f1e913d3b0145d3e286deb477bb8
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2025-03-31 16:39:49 +0200

    Move supports to block.json: Product Elements Stock Indicator

--- plugins/woocommerce/client/blocks/assets/js/atomic/blocks/product-elements/stock-indicator/index.ts
+++ plugins/woocommerce/client/blocks/assets/js/atomic/blocks/product-elements/stock-indicator/index.ts
@@ -10,7 +10,6 @@ import type { BlockConfiguration } from '@wordpress/blocks';
 import sharedConfig from '../shared/config';
 import edit from './edit';
 import metadata from './block.json';
-import { supports } from './supports';
 
 import { BLOCK_ICON as icon } from './constants';
 
@@ -18,7 +17,6 @@ const blockConfig: BlockConfiguration = {
 	...metadata,
 	...sharedConfig,
 	icon: { src: icon },
-	supports,
 	edit,
 	save: () => null,
 };
