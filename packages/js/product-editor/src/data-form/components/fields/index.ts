dcb3d33470aff203ddc6bcd1a6f52fa9b38571ab
Manish Menaria <the.manish.menaria@gmail.com>
2025-04-02 11:34:08 +0530

    POC: Add Regular and Sale fields to Product Editor (#56912)
    * Add regular price field to product editor
    
    * Add Sale Price field to the product editor

--- packages/js/product-editor/src/data-form/components/fields/index.ts
+++ packages/js/product-editor/src/data-form/components/fields/index.ts
@@ -2,9 +2,13 @@
  * Internal dependencies
  */
 import { initNameField } from './name';
+import { initRegularPriceField } from './regular-price';
+import { initSalePriceField } from './sale-price';
 
 export function initFields() {
 	initNameField();
+	initRegularPriceField();
+	initSalePriceField();
 }
 
 export * from './registration';
