8c2ecad63bc3cff9b26df9d49c52b37df6f53ad5
Chris Lilitsas <1105590+xristos3490@users.noreply.github.com>
2025-04-08 23:28:12 +0300

    Lint fixes

--- plugins/woocommerce/src/Internal/DataStores/StockNotifications/StockNotificationsMetaDataStore.php
+++ plugins/woocommerce/src/Internal/DataStores/StockNotifications/StockNotificationsMetaDataStore.php
@@ -3,7 +3,7 @@
  * StockNotificationsMetaDataStore class file.
  */
 
- declare( strict_types = 1 );
+declare( strict_types = 1 );
 
 namespace Automattic\WooCommerce\Internal\DataStores\StockNotifications;
 
