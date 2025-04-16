b91a581d3e2a2713ceb3f9a03395a8b833e861b3
Thomas Roberts <5656702+opr@users.noreply.github.com>
2025-04-16 18:47:25 +0100

    Add a WC_Address_Provider abstract class

---
+++ plugins/woocommerce/includes/abstracts/abstract-wc-address-provider.php
@@ -0,0 +1,32 @@
+<?php
+declare( strict_types=1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * WooCommerce Address Provider Class.
+ *
+ * Extended by address providers to handle address provision, for autocomplete, maps, etc.
+ *
+ * @class       WC_Address_Provider
+ * @version     10.2.0
+ * @package     WooCommerce\Abstracts
+ */
+abstract class WC_Address_Provider {
+
+	/**
+	 * Unique ID for the address provider - must be set.
+	 *
+	 * @var string
+	 */
+	public $id = '';
+
+	/**
+	 * Provider name.
+	 *
+	 * @var string
+	 */
+	public $name = '';
+}
