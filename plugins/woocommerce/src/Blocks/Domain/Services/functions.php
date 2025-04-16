af17c34574fd5b37fe7a516cfe4cb7944087ec4f
Thomas Roberts <5656702+opr@users.noreply.github.com>
2025-04-16 18:39:48 +0100

    Remove deregister helper function

--- plugins/woocommerce/src/Blocks/Domain/Services/functions.php
+++ plugins/woocommerce/src/Blocks/Domain/Services/functions.php
@@ -94,16 +94,3 @@ if ( ! function_exists( '__experimental_woocommerce_register_address_provider' )
 		}
 	}
 }
-
-if ( ! function_exists( '__experimental_woocommerce_deregister_address_autocomplete_provider' ) ) {
-	/**
-	 * Deregister an address autocomplete provider.
-	 *
-	 * @param string $provider_id Provider ID.
-	 * @internal
-	 */
-	function __experimental_woocommerce_deregister_address_autocomplete_provider( string $provider_id ) { // phpcs:ignore WordPress.NamingConventions.ValidFunctionName.FunctionDoubleUnderscore,PHPCompatibility.FunctionNameRestrictions.ReservedFunctionNames.FunctionDoubleUnderscore
-		$address_autocomplete = Package::container()->get( AddressAutocomplete::class );
-		$address_autocomplete->deregister_provider( $provider_id );
-	}
-}
