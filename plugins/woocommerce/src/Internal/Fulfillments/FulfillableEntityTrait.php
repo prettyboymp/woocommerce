615bdfd0d2244bebab234d01e38227ae7c588c08
Taha Paksu <3295+tpaksu@users.noreply.github.com>
2025-04-28 14:34:18 +0300

    Make orders fulfillable entities with a trait

---
+++ plugins/woocommerce/src/Internal/Fulfillments/FulfillableEntityTrait.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ * Fulfillable trait.
+ */
+
+declare( strict_types=1 );
+
+namespace Automattic\WooCommerce\Internal\Fulfillments;
+
+use Automattic\WooCommerce\Internal\DataStores\Fulfillments\FulfillmentsDataStore;
+
+/**
+ * This trait is used to mark classes that can be fulfilled.
+ *
+ * @since 9.0.0
+ */
+trait FulfillableEntityTrait {
+	/**
+	 * Fulfillable ID, which is the identifier of the object with fulfillments. For example, `WC_Order::get_id()` is used for order fulfillments.
+	 *
+	 * @var string
+	 */
+	abstract public function get_fulfillable_id(): string;
+
+	/**
+	 * Fulfillable type, which is the identifier of the object with fulfillments. For example, `WC_Order::class` is used for order fulfillments.
+	 *
+	 * @var string
+	 */
+	abstract public function get_fulfillable_type(): string;
+
+	/**
+	 * Fulfillable status, which is the status of the object with fulfillments. For example, `WC_Order::get_status()` is used for order fulfillments.
+	 *
+	 * @param string $status Fulfillable status.
+	 *
+	 * @var string
+	 */
+	abstract public function set_fulfillment_status( string $status ): void;
+
+	/**
+	 * Fulfillable status, which is the status of the object with fulfillments. For example, `WC_Order::get_status()` is used for order fulfillments.
+	 *
+	 * @var string
+	 */
+	abstract public function get_fulfillment_status(): string;
+
+	/**
+	 * Get fulfillments of an entity.
+	 *
+	 * @return Array<Fulfillment>
+	 */
+	public function get_fulfillments(): array {
+		return wc_get_container()
+			->get( FulfillmentsDataStore::class )
+			->read_fulfillments( $this->get_fulfillable_type(), $this->get_fulfillable_id() );
+	}
+
+	/**
+	 * Create a new fulfillment on the entity.
+	 *
+	 * @param Fulfillment $fulfillment Fulfillment object.
+	 *
+	 * @return void
+	 */
+	public function add_fulfillment( Fulfillment $fulfillment ): void {
+		wc_get_container()
+			->get( FulfillmentsDataStore::class )
+			->create( $fulfillment );
+		$this->update_order_fulfillment_status();
+	}
+
+	/**
+	 * Update an existing fulfillment on the entity.
+	 *
+	 * @param Fulfillment $fulfillment Fulfillment object.
+	 *
+	 * @return void
+	 */
+	public function update_fulfillment( Fulfillment $fulfillment ): void {
+		wc_get_container()
+			->get( FulfillmentsDataStore::class )
+			->update( $fulfillment );
+		$this->update_order_fulfillment_status();
+	}
+
+	/**
+	 * Delete a fulfillment from the entity.
+	 *
+	 * @param Fulfillment $fulfillment Fulfillment object.
+	 *
+	 * @return void
+	 */
+	public function delete_fulfillment( Fulfillment $fulfillment ): void {
+		wc_get_container()
+			->get( FulfillmentsDataStore::class )
+			->delete( $fulfillment );
+		$this->update_order_fulfillment_status();
+	}
+
+	/**
+	 * Mark a fulfillment as fulfilled.
+	 *
+	 * @param Fulfillment $fulfillment Fulfillment object.
+	 * @param string      $status     Fulfillment status.
+	 *
+	 * @return void
+	 */
+	public function fulfill( Fulfillment $fulfillment, string $status = 'fulfilled' ): void {
+		$fulfillment->set_status( $status );
+		$fulfillment->set_is_fulfilled( true );
+		$fulfillment->save();
+		$this->update_order_fulfillment_status();
+	}
+
+	/**
+	 * Update the fulfillment status of the entity based on the fulfillments.
+	 *
+	 * This method checks if all fulfillments are fulfilled, if some fulfillments are fulfilled, or if no fulfillments are present.
+	 * It sets the fulfillment status of the entity accordingly.
+	 */
+	private function update_order_fulfillment_status(): void {
+		$fulfillments     = $this->get_fulfillments();
+		$has_fulfillments = ! empty( $fulfillments );
+		$all_fulfilled    = true;
+		$some_fulfilled   = false;
+
+		if ( $has_fulfillments ) {
+			foreach ( $fulfillments as $fulfillment ) {
+				if ( ! $fulfillment->get_is_fulfilled() ) {
+					$all_fulfilled = false;
+				} else {
+					$some_fulfilled = true;
+				}
+			}
+
+			if ( $all_fulfilled ) {
+				$this->set_fulfillment_status( 'fulfilled' );
+			} elseif ( $some_fulfilled ) {
+				$this->set_fulfillment_status( 'partially_fulfilled' );
+			} else {
+				$this->set_fulfillment_status( 'unfulfilled' );
+			}
+		} else {
+			$this->set_fulfillment_status( 'no_fulfillments' );
+		}
+	}
+}
---
+++ plugins/woocommerce/tests/php/src/Internal/Fulfillments/FulfillableEntityTraitTest.php
@@ -0,0 +1,210 @@
+<?php
+/**
+ * FulfillableEntityTraitTest
+ *
+ * @package Automattic\WooCommerce\Tests\Internal\Fulfillments
+ */
+
+declare( strict_types=1 );
+
+namespace Automattic\WooCommerce\Tests\Internal\Fulfillments;
+
+use Automattic\WooCommerce\RestApi\UnitTests\Helpers\OrderHelper;
+use Automattic\WooCommerce\Internal\Fulfillments\FulfillableEntityTrait;
+use Automattic\WooCommerce\Internal\Fulfillments\Fulfillment;
+use WC_Order;
+
+/**
+ * This class tests the FulfillableEntityTrait.
+ */
+class FulfillableEntityTraitTest extends \WP_UnitTestCase {
+
+	/**
+	 * @var int
+	 */
+	protected $order_id;
+
+	/**
+	 * @var WC_Order
+	 */
+	protected $order;
+
+	/**
+	 * Set up the test case.
+	 */
+	public function setUp(): void {
+		parent::setUp();
+		$this->order = OrderHelper::create_order();
+	}
+
+	/**
+	 * Test if the order has the FulfillableEntityTrait trait.
+	 */
+	public function test_if_order_has_fulfillable_entity_trait() {
+		$this->assertTrue(
+			trait_exists( FulfillableEntityTrait::class ),
+			'The FulfillableEntityTrait trait does not exist.'
+		);
+		$this->assertTrue(
+			in_array(
+				FulfillableEntityTrait::class,
+				$this->class_uses_deep( $this->order ),
+				true
+			),
+			'The order does not use the FulfillableEntityTrait trait.'
+		);
+	}
+
+	/**
+	 * Test the get_fulfillable_id method.
+	 */
+	public function test_get_fulfillable_id() {
+		$this->assertEquals( (string) $this->order->get_id(), $this->order->get_fulfillable_id() );
+	}
+
+	/**
+	 * Test the get_fulfillable_type method.
+	 */
+	public function test_get_fulfillable_type() {
+		$this->assertEquals( WC_Order::class, $this->order->get_fulfillable_type() );
+	}
+
+	/**
+	 * Test the set_fulfillment_status method.
+	 */
+	public function test_get_set_fulfillment_status() {
+		$this->order->set_fulfillment_status( 'unfulfilled' );
+		$this->assertEquals( 'unfulfilled', $this->order->get_fulfillment_status() );
+	}
+
+	/**
+	 * Test the get_fulfillments method.
+	 */
+	public function test_add_get_fulfillments() {
+		$fulfillment = new Fulfillment();
+		$fulfillment->set_entity_id( (string) $this->order->get_id() );
+		$fulfillment->set_entity_type( WC_Order::class );
+		$fulfillment->set_status( 'unfulfilled' );
+		$fulfillment->set_items(
+			array(
+				array(
+					'item_id' => 1,
+					'qty'     => 2,
+				),
+			)
+		);
+
+		$this->order->add_fulfillment( $fulfillment );
+
+		$fulfillments = $this->order->get_fulfillments();
+
+		$this->assertCount( 1, $fulfillments );
+		$this->assertEquals( 'unfulfilled', $fulfillments[0]->get_status() );
+	}
+
+	/**
+	 * Test the update_fulfillment method.
+	 */
+	public function test_update_fulfillment() {
+		$fulfillment = new Fulfillment();
+		$fulfillment->set_entity_id( (string) $this->order->get_id() );
+		$fulfillment->set_entity_type( WC_Order::class );
+		$fulfillment->set_status( 'unfulfilled' );
+		$fulfillment->set_items(
+			array(
+				array(
+					'item_id' => 1,
+					'qty'     => 2,
+				),
+			)
+		);
+
+		$this->order->add_fulfillment( $fulfillment );
+
+		$fulfillment->set_status( 'fulfilled' );
+		$this->order->update_fulfillment( $fulfillment );
+
+		$fulfillments = $this->order->get_fulfillments();
+		$this->assertCount( 1, $fulfillments );
+		$this->assertEquals( 'fulfilled', $fulfillments[0]->get_status() );
+	}
+
+	/**
+	 * Test order fulfillment status when no fulfillments.
+	 */
+	public function test_order_fulfillment_status_no_fulfillments() {
+		$this->assertEquals( 'no_fulfillments', $this->order->get_fulfillment_status() );
+	}
+
+	/**
+	 * Test order fulfillment status updates.
+	 */
+	public function test_update_order_fulfillment_status() {
+		$fulfillment = new Fulfillment();
+		$fulfillment->set_entity_id( (string) $this->order->get_id() );
+		$fulfillment->set_entity_type( WC_Order::class );
+		$fulfillment->set_status( 'unfulfilled' );
+		$fulfillment->set_is_fulfilled( false );
+		$fulfillment->set_items(
+			array(
+				array(
+					'item_id' => 1,
+					'qty'     => 2,
+				),
+			)
+		);
+
+		$this->order->add_fulfillment( $fulfillment );
+
+		$fulfillment_2 = new Fulfillment();
+		$fulfillment_2->set_entity_id( (string) $this->order->get_id() );
+		$fulfillment_2->set_entity_type( WC_Order::class );
+		$fulfillment_2->set_status( 'unfulfilled' );
+		$fulfillment_2->set_is_fulfilled( false );
+		$fulfillment_2->set_items(
+			array(
+				array(
+					'item_id' => 1,
+					'qty'     => 2,
+				),
+			)
+		);
+
+		$this->order->add_fulfillment( $fulfillment_2 );
+
+		$this->assertEquals( 'unfulfilled', $this->order->get_fulfillment_status() );
+
+		$fulfillment->set_status( 'fulfilled' );
+		$fulfillment->set_is_fulfilled( true );
+		$this->order->update_fulfillment( $fulfillment );
+
+		$this->assertEquals( 'partially_fulfilled', $this->order->get_fulfillment_status() );
+
+		$fulfillment_2->set_status( 'fulfilled' );
+		$fulfillment_2->set_is_fulfilled( true );
+		$this->order->update_fulfillment( $fulfillment_2 );
+
+		$this->assertEquals( 'fulfilled', $this->order->get_fulfillment_status() );
+	}
+
+	/**
+	 * Utility function to get all traits used by a class and its parent classes.
+	 *
+	 * @param class-string|object $object_or_class The class name.
+	 * @param bool                $autoload Whether to autoload the class if not found.
+	 *
+	 * @return array An array of trait names.
+	 */
+	private function class_uses_deep( $object_or_class, bool $autoload = true ): array {
+		$traits = class_uses( $object_or_class, $autoload );
+		$parent = get_parent_class( $object_or_class );
+		if ( $parent ) {
+			$traits = array_merge( $traits, $this->class_uses_deep( $parent, $autoload ) );
+		}
+
+		foreach ( $traits as $trait ) {
+			$traits = array_merge( $traits, $this->class_uses_deep( $trait, $autoload ) );
+		}
+		return $traits;
+	}
+}
