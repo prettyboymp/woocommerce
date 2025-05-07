e9ade714b516cf7b88ac6dbf481d4ae8d440d8ff
Luigi Teschio <gigitux@gmail.com>
2025-05-07 12:04:25 +0200

    improve unit test

--- plugins/woocommerce/tests/php/src/Blocks/Patterns/PTKClientTest.php
+++ plugins/woocommerce/tests/php/src/Blocks/Patterns/PTKClientTest.php
@@ -147,10 +147,10 @@ class PTKClientTest extends \WP_UnitTestCase {
 		remove_filter( 'pre_http_request', array( $this, 'return_request_failed' ) );
 	}
 
-		/**
-		 * Test fetch_patterns returns an array of patterns when the request is successful.
-		 */
-	public function test_fetch_patterns_returns_an_array_of_patterns_when_the_request_is_successful_with_invalid_payload() {
+	/**
+	 * Test fetch_patterns returns an WP_Error when the request is successful with one item with invalid payload.
+	 */
+	public function test_fetch_patterns_returns_an_wp_error_when_the_request_is_successful_with_invalid_payload() {
 		add_filter(
 			'pre_http_request',
 			function () {
@@ -170,6 +170,51 @@ class PTKClientTest extends \WP_UnitTestCase {
                                 }
                             }
                         },
+						{
+                            "ID": 14870,
+                            "site_id": 174455321,
+                            "title": "Review: A quote with scattered images",
+                            "name": "review-a-quote-with-scattered-images",
+                            "html": "<!-- /wp:spacer -->",
+                            "categories": null,
+                        }
+                    ]',
+					'response' => array(
+						'code' => 200,
+					),
+				);
+			},
+		);
+
+		$response = $this->client->fetch_patterns();
+		$this->assertErrorResponse( $response, 'Wrong response received from the Patterns Toolkit API: try again later.' );
+
+		remove_filter( 'pre_http_request', array( $this, 'return_request_failed' ) );
+	}
+
+	/**
+	 * Test fetch_patterns returns an WP_Error when the request is successful with one item with invalid payload.
+	 */
+	public function test_fetch_patterns_returns_an_wp_error_when_the_request_is_successful_with_one_item_with_invalid_payload() {
+		add_filter(
+			'pre_http_request',
+			function () {
+				return array(
+					'body'     => '[
+                        {
+                            "ID": 14870,
+                            "site_id": 174455321,
+                            "title": "Review: A quote with scattered images",
+                            "name": "review-a-quote-with-scattered-images",
+                            "html": "<!-- /wp:spacer -->",
+                            "categories": {
+                                "testimonials": {
+                                    "slug": "testimonials",
+                                    "title": "Testimonials",
+                                    "description": "Share reviews and feedback about your brand/business."
+                                }
+                            }
+                        }
 						{
                             "ID": 14870,
                             "site_id": 174455321,
