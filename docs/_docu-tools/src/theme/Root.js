f998daac758f87465f269a10521dfd0cf3fefc5f
piinthecloud <stephanie.pi@automattic.com>
2025-05-09 17:35:39 +0200

    add cookie banner

--- docs/_docu-tools/src/theme/Root.js
+++ docs/_docu-tools/src/theme/Root.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import CookieConsent, { Cookies, getCookieConsentValue } from "react-cookie-consent";
+import { CookieBanner } from '../components/CookieBanner';
 
-export default function Root({children}) {
+export default function Root({ children }) {
   return (
     <>
-      <CookieConsent
-        location="bottom"
-        buttonText="I agree"
-        enableDeclineButton
-        setDeclineCookie={true}
-        cookieName="cookie-consent"
-        style={{ background: "#d1c1ff", color: "#000" }}
-        buttonStyle={{
-            background: "#720eec",
-            color: "#fff",
-          }}
-      >
-        We use Google Analytics to understand how visitors interact with our documentation. This helps us improve your experience.
-      </CookieConsent>
       {children}
+      <CookieBanner />
     </>
   );
 }
