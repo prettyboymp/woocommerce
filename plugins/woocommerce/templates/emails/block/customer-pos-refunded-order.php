bf8c85f82d4d70559130a51c785ef6d67f22c1cf
Jaclyn Chen <watertranquil@gmail.com>
2025-05-13 14:28:22 +0800

    Remove unnecessary content in POS refund email block template that is moved to general-block-email.

--- plugins/woocommerce/templates/emails/block/customer-pos-refunded-order.php
+++ plugins/woocommerce/templates/emails/block/customer-pos-refunded-order.php
@@ -30,37 +30,6 @@ printf( esc_html__( 'Order refunded: %s', 'woocommerce' ), '<!--[woocommerce/ord
 </h2>
 <!-- /wp:heading -->
 
-<!-- wp:paragraph -->
-<p>
-<?php
-	/* translators: %s: Customer first name */
-	printf( esc_html__( 'Hi %s,', 'woocommerce' ), '<!--[woocommerce/shopper-first-name]-->' );
-?>
-</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p>
-<?php
-/* translators: %s: Site title */
-printf( esc_html__( 'Your order from %s has been refunded.', 'woocommerce' ), '<!--[woocommerce/site-title]-->' );
-?>
-</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p> <?php echo esc_html__( 'Here’s a reminder of what you’ve ordered:', 'woocommerce' ); ?> </p>
-<!-- /wp:paragraph -->
-
 <!-- wp:woo/email-content {"lock":{"move":false,"remove":true}} -->
 <div class="wp-block-woo-email-content"> <?php echo esc_html( BlockEmailRenderer::WOO_EMAIL_CONTENT_PLACEHOLDER ); ?> </div>
 <!-- /wp:woo/email-content -->
-
-<!-- wp:paragraph -->
-<p>
-<?php
-/* translators: %s: Store admin email */
-printf( esc_html__( 'If you need any help with your order, please contact us at %s', 'woocommerce' ), '<!--[woocommerce/store-email]-->' );
-?>
-</p>
-<!-- /wp:paragraph -->
