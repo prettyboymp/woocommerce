6db51ba44809d7751ae5d79bd0b1c1d4ba346aa8
Panos (Panagiotis) Synetos <2484390+PanosSynetos@users.noreply.github.com>
2025-05-21 15:35:14 +0300

    Improves product availability checks for variations.
    Improves performance of product availability checks for variable products by fetching variation objects instead of full variation data when determining if variations are available.
    
    Adds product object to the woocommerce_product_availability filter to allow more context.

--- plugins/woocommerce/src/Blocks/Utils/ProductAvailabilityUtils.php
+++ plugins/woocommerce/src/Blocks/Utils/ProductAvailabilityUtils.php
@@ -28,7 +28,7 @@ class ProductAvailabilityUtils {
 		// If the product is a variable product, check if it has any available variations.
 		// We will show a custom availability message if it does.
 		if ( $product->get_type() === ProductType::VARIABLE ) {
-			$available_variations = $product->get_available_variations();
+			$available_variations = $product->get_available_variations( 'objects' );
 			if ( empty( $available_variations ) && false !== $available_variations ) {
 				$product_availability['availability'] = __( 'This product is currently out of stock and unavailable.', 'woocommerce' );
 				$product_availability['class']        = 'out-of-stock';
@@ -40,9 +40,10 @@ class ProductAvailabilityUtils {
 		/**
 		 * Filters the product availability information.
 		 *
-		 * @since 9.7.0
+		 * @since x.x.x
 		 * @param array $product_availability The product availability information.
+		 * @param \WC_Product $product Product object.
 		 */
-		return apply_filters( 'woocommerce_product_availability', $product_availability );
+		return apply_filters( 'woocommerce_product_availability', $product_availability, $product );
 	}
 }
