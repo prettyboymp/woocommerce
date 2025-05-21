c82bee0d9037dfde0659d83bcf505c75461b0f0f
Peter Fabian <peter.fabian.github@gmail.com>
2025-05-21 14:09:06 +0200

    Added clearer information about the required format for reporting parameters.

--- plugins/woocommerce/src/Admin/API/Reports/Variations/Controller.php
+++ plugins/woocommerce/src/Admin/API/Reports/Variations/Controller.php
@@ -306,6 +306,9 @@ class Controller extends GenericController implements ExportableInterface {
 			'type'              => 'array',
 			'items'             => array(
 				'type' => 'array',
+				'items' => array(
+					'type' => 'integer',
+				),
 			),
 			'default'           => array(),
 			'validate_callback' => 'rest_validate_request_arg',
@@ -315,6 +318,9 @@ class Controller extends GenericController implements ExportableInterface {
 			'type'              => 'array',
 			'items'             => array(
 				'type' => 'array',
+				'items' => array(
+					'type' => 'integer',
+				),
 			),
 			'default'           => array(),
 			'validate_callback' => 'rest_validate_request_arg',
