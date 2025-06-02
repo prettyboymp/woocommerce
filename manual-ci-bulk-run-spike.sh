5b6e3f7452b7ce074a8ad87eafda14f5a17ecb47
Vladimir Reznichenko <kalessil@gmail.com>
2025-06-02 15:48:12 +0200

    Release: WIP, canonical extensions testing.

--- manual-ci-bulk-run-spike.sh
+++ manual-ci-bulk-run-spike.sh
@@ -28,22 +28,22 @@ done
 # Run checks for the target repositories.
 echo "Launching checks (${#filtered[@]} repos)"
 for repository in ${filtered[@]}; do
-	echo -n "    -- $repository: "
+	echo -n "    -- $repository:"
 
 	# Report identified workflow details.
 	workflow=$( gh workflow list --json path,id --repo $repository | jq --compact-output '.[]' | grep -E '.github/workflows/(manual-ci.yml|ci-manual.yml)' )
 	workflow_path=$( echo $workflow | jq --raw-output '( .path )' )
 	workflow_id=$( echo $workflow | jq --raw-output '( .id )' )
-	echo -n "workflow ${workflow_path}(${workflow_id}) "
+	echo -n " workflow ${workflow_path}(${workflow_id})"
 
 	# Report last run details.
 	previous_run=$( gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /repos/${repository/"https://github.com/"/}/actions/workflows/${workflow_id}/runs?per_page=1 --jq '.workflow_runs.[].id' )
-	echo -n " previous run #${previous_run} "
+	echo -n " previous run #${previous_run}"
 
 	# Start a new run and report back.
-	echo '{"wc-version":"9.9.0-rc.1", "qit-tests":"WooCommerce Pre-Release Tests (includes Activation, WooCommerce E2E and API tests)"}' | gh workflow run ${workflow_id} --json
+	echo '{"wc-version":"9.9.0-rc.1", "qit-tests":"WooCommerce Pre-Release Tests (includes Activation, WooCommerce E2E and API tests)"}' | gh workflow run ${workflow_id} --json --repo $repository >/dev/null 2>&1
 	for i in {1..10}; do
-	    echo -n '.' && sleep 1s
+	    echo -n ' .' && sleep 1s
 	    last_run=$( gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /repos/${repository/"https://github.com/"/}/actions/workflows/${workflow_id}/runs?per_page=1 --jq '.workflow_runs.[].id' )
 	    if [[ $last_run != $previous_run ]]; then
 	    	echo -n " running #${last_run}"
