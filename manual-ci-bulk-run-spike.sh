1e553b90b984415a5af95331a8437792230f3d04
Vladimir Reznichenko <kalessil@gmail.com>
2025-06-03 09:59:09 +0200

    Release: WIP, canonical extensions testing.

--- manual-ci-bulk-run-spike.sh
+++ manual-ci-bulk-run-spike.sh
@@ -26,6 +26,7 @@ for repository in ${skipped[@]}; do
 done
 
 # Run checks for the target repositories.
+running=()
 echo "Launching checks (${#filtered[@]} repo(s))"
 for repository in ${filtered[@]}; do
 	echo -n "    -- $repository:"
@@ -38,18 +39,36 @@ for repository in ${filtered[@]}; do
 
 	# Report last run details.
 	previous_run=$( gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /repos/${repository/"https://github.com/"/}/actions/workflows/${workflow_id}/runs?per_page=1 --jq '.workflow_runs.[].id' )
-	echo -n " previous run #${previous_run}"
+	echo -n " previous run #${previous_run} "
 
 	# Start a new run and report back.
 	echo '{"wc-version":"9.9.0-rc.1", "qit-tests":"WooCommerce Pre-Release Tests (includes Activation, WooCommerce E2E and API tests)"}' | gh workflow run ${workflow_id} --json --repo $repository >/dev/null 2>&1
 	for i in {1..10}; do
-	    echo -n ' .' && sleep 1s
+	    echo -n '.' && sleep 1s
 	    last_run=$( gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /repos/${repository/"https://github.com/"/}/actions/workflows/${workflow_id}/runs?per_page=1 --jq '.workflow_runs.[].id' )
 	    if [[ $last_run != $previous_run ]]; then
+	    	running+=( "$repository;${last_run}" )
 	    	echo -n " running #${last_run}"
 	    	break
 	    fi
 	done
 
 	echo ''
+done
+
+echo "Waiting for completion (${#running[@]} run(s), 1m check interval): "
+while [ ${#running[@]} -gt 0 ]; do
+	echo -n '.' && sleep 1m
+	updated=()
+	for entry in ${running[@]}; do
+		fragments=( ${entry//;/ } )
+		repository=${fragments[0]}
+		id=${fragments[1]}
+		status=$( gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /repos/${repository/"https://github.com/"/}/actions/runs/$id --jq '.status' )
+		if [[ $status == 'completed' ]] || [[ $status == 'failure' ]] || [[ $status == 'startup_failure' ]]; then
+			updated+=( $entry )
+			echo -n '*'
+		fi
+	done
+	running=( "${updated[@]}" )
 done
