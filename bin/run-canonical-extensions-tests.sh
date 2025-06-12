5e054e3a703a8138fd409ae650fff349ffe24e45
Vladimir Reznichenko <kalessil@gmail.com>
2025-06-12 08:50:03 +0200

    Release: new fixes

--- bin/run-canonical-extensions-tests.sh
+++ bin/run-canonical-extensions-tests.sh
@@ -18,7 +18,7 @@ filtered=()
 skipped=()
 echo -n "Looking up repositories (${#repositories[@]}): "
 for repository in ${repositories[@]}; do
-	match=$( ( gh workflow list --json path --jq '.[].path' --repo $repository | grep -E '.github/workflows/(manual-ci.yml|ci-manual.yml)' | wc -l ) || echo '0' )
+	match=$( ( gh workflow list --json path --jq '.[].path' --repo $repository | grep -E '.github/workflows/(manual-ci.yml|ci-manual.yml)' | wc -l | tr -d '[:space:]' ) || echo '0' )
 	if [[ $match == '1' ]]; then
 		filtered+=( $repository )
 	else
@@ -49,14 +49,14 @@ for repository in ${filtered[@]}; do
 	echo -n " workflow ${workflow_path} (#${workflow_id})"
 
 	# Report last run details.
-	previous_run=$( gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /repos/${repository/"https://github.com/"/}/actions/workflows/${workflow_id}/runs?per_page=1 --jq '.workflow_runs.[].id' )
+	previous_run=$( gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /repos/${repository##https://github.com/}/actions/workflows/${workflow_id}/runs?per_page=1 --jq '.workflow_runs.[].id' )
 	echo -n " previous run #${previous_run} "
 
 	# Start a new run and report back.
 	echo "{\"wc-version\":\"$version\", \"qit-tests\":\"WooCommerce Pre-Release Tests (includes Activation, WooCommerce E2E and API tests)\"}" | gh workflow run ${workflow_id} --json --repo $repository >/dev/null
 	for i in {1..10}; do
 	    echo -n '.' && sleep 1s
-	    last_run=$( gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /repos/${repository/"https://github.com/"/}/actions/workflows/${workflow_id}/runs?per_page=1 --jq '.workflow_runs.[].id' )
+	    last_run=$( gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /repos/${repository##https://github.com/}/actions/workflows/${workflow_id}/runs?per_page=1 --jq '.workflow_runs.[].id' )
 	    if [[ $last_run != $previous_run ]]; then
             running+=( "$repository;${last_run}" )
             echo -n " new run #${last_run}"
@@ -76,11 +76,11 @@ while [ ${#running[@]} -gt 0 ]; do
 		fragments=( ${entry//;/ } )
 		repository=${fragments[0]}
 		id=${fragments[1]}
-		status=$( gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /repos/${repository/"https://github.com/"/}/actions/runs/$id --jq '.status' )
+		status=$( gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /repos/${repository##https://github.com/}/actions/runs/$id --jq '.status' )
 		# https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/collaborating-on-repositories-with-code-quality-features/about-status-checks#check-statuses-and-conclusions
 		if [[ $status == 'completed' ]] || [[ $status == 'failure' ]] || [[ $status == 'startup_failure' ]]; then
 			if [[ $status == 'completed' ]]; then
-				conclusion=$( gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /repos/${repository/"https://github.com/"/}/actions/runs/$id --jq '.conclusion' )
+				conclusion=$( gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /repos/${repository##https://github.com/}/actions/runs/$id --jq '.conclusion' )
 				status="$status:$conclusion"
 			fi
 			echo -n '✓'
